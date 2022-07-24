f48a277ec23d038762ebcf620953755b9878a702
onkar <onkar.k@mplussoft.in>
2022-07-24 10:27:45 +0530

    fix layout

--- android/src/main/java/com/amolg/flutterbarcodescanner/BarcodeCaptureActivity.java
+++ android/src/main/java/com/amolg/flutterbarcodescanner/BarcodeCaptureActivity.java
@@ -125,7 +125,7 @@ public final class BarcodeCaptureActivity extends AppCompatActivity implements B
         imgViewBarcodeCaptureUseFlash.setOnClickListener(this);
         imgViewBarcodeCaptureUseFlash.setVisibility(FlutterBarcodeScannerPlugin.isShowFlashIcon ? View.VISIBLE : View.GONE);
 
-        imgViewSwitchCamera = findViewById(R.id.imgViewSwitchCamera);
+//        imgViewSwitchCamera = findViewById(R.id.imgViewSwitchCamera);
         imgViewSwitchCamera.setOnClickListener(this);
 
         mPreview = findViewById(R.id.preview);
@@ -417,13 +417,14 @@ public final class BarcodeCaptureActivity extends AppCompatActivity implements B
             barcode.displayValue = "-1";
             FlutterBarcodeScannerPlugin.onBarcodeScanReceiver(barcode);
             finish();
-        } else if (i == R.id.imgViewSwitchCamera) {
-            int currentFacing = mCameraSource.getCameraFacing();
-            boolean autoFocus = mCameraSource.getFocusMode() != null;
-            boolean useFlash = flashStatus == USE_FLASH.ON.ordinal();
-            createCameraSource(autoFocus, useFlash, getInverseCameraFacing(currentFacing));
-            startCameraSource();
         }
+//        else if (i == R.id.imgViewSwitchCamera) {
+//            int currentFacing = mCameraSource.getCameraFacing();
+//            boolean autoFocus = mCameraSource.getFocusMode() != null;
+//            boolean useFlash = flashStatus == USE_FLASH.ON.ordinal();
+//            createCameraSource(autoFocus, useFlash, getInverseCameraFacing(currentFacing));
+//            startCameraSource();
+//        }
     }
 
     private int getInverseCameraFacing(int cameraFacing) {
